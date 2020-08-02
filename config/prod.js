3ca27e7c363b5168b8ae4c78ba094ce3cd6cb577
antimattercorrade <kush991013@gmail.com>
2020-08-02 10:24:35 +0530

    second commit

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
-    MONGOURI:"mongodb+srv://instagram:QNcBhD5jRX87oVV5@cluster0.v01bc.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    JWT_KEY:"sdfjhsfghsdgtyfd",
+    MONGOURI:process.env.MONGOURI,
+    JWT_KEY:process.env.JWT_KEY,
     SENDGRID_API:process.env.SENDGRID_API,
     EMAIL:process.env.EMAIL
 }
