ed436288251fcb3f4425b31d7ca3003163ff6d51
antimattercorrade <kush991013@gmail.com>
2020-08-02 10:22:33 +0530

    first commit

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    MONGOURI:"mongodb+srv://instagram:QNcBhD5jRX87oVV5@cluster0.v01bc.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    JWT_KEY:"sdfjhsfghsdgtyfd",
+    SENDGRID_API:process.env.SENDGRID_API,
+    EMAIL:process.env.EMAIL
+}
---
+++ routes/auth.js
@@ -0,0 +1,137 @@
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
+const User = mongoose.model("User")
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const {JWT_KEY} = require('../config/keys')
+const requireLogin = require('../middleware/requireLogin')
+const crypto = require('crypto')
+const nodemailer = require('nodemailer')
+const sendgridTransport = require('nodemailer-sendgrid-transport')
+const {SENDGRID_API,EMAIL} = require('../config/keys')
+mongoose.set('useFindAndModify', false);
+
+const transporter = nodemailer.createTransport(sendgridTransport({
+    auth:{
+        api_key:SENDGRID_API
+    }
+}))
+
+
+router.post('/signup',(req,res) => {
+    const{name,email,password,pic} = req.body
+    if(!email || !password || !name){
+        return res.status(422).json({error:"Please fill all the fields"})
+    }
+    User.findOne({email:email})
+    .then((savedUser) => {
+        if(savedUser){
+            return res.status(422).json({error:"Email already exists"})
+    
+        }
+
+        bcrypt.hash(password,12)
+        .then(hashedpassword => {
+            const user = new User({
+                email:email,
+                password:hashedpassword,
+                name:name,
+                pic:pic
+            })
+    
+            user.save()
+            .then(user => {
+                res.json({message: "Saved Successfully"})
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        })
+
+        
+    })
+    .catch(err => {
+        console.log(err)
+    })
+})
+
+router.post('/signin',(req,res) => {
+    const {email,password} = req.body
+    if(!email || !password){
+        return res.status(422).json({error:"Please Provide Email and Password"})
+    }
+    User.findOne({email:email})
+    .then(savedUser => {
+        if(!savedUser){
+            return res.status(422).json({error:"Invalid Email or Password"})
+        }
+        bcrypt.compare(password,savedUser.password)
+        .then(doMatch => {
+            if(doMatch){
+                const token = jwt.sign({_id:savedUser._id}, JWT_KEY)
+                const {_id,name,email,followers,following,pic} = savedUser
+                res.json({token:token, user:{_id,name,email,followers,following,pic}})
+            }
+            else {
+                return res.status(422).json({error:"Invalid Email or Password"})
+            }
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    })
+})
+
+router.post('/reset-password',(req,res)=>{
+    crypto.randomBytes(32,(err,buffer)=>{
+        if(err){
+            console.log(err)
+        }
+        const token = buffer.toString("hex")
+        User.findOne({email:req.body.email})
+        .then(user=>{
+            if(!user){
+                return res.status(422).json({error:"User Doesn't Exist"})
+            }
+            user.resetToken = token
+            user.expireToken = Date.now() + 3600000
+            user.save().then((result)=>{
+                transporter.sendMail({
+                    to:user.email,
+                    from:"no-replay@insta.com",
+                    subject:"Password Reset",
+                    html:`
+                    <p>You requested for password reset</p>
+                    <h5>Click on this <a href="${EMAIL}/reset/${token}">link</a> to reset password</h5>
+                    `
+                })
+                res.json({message:"Check Your Email"})
+            })
+
+        })
+    })
+})
+
+router.post('/new-password',(req,res)=>{
+    const newPassword = req.body.password
+    const sentToken = req.body.token
+    User.findOne({resetToken:sentToken,expireToken:{$gt:Date.now()}})
+    .then(user=>{
+        if(!user){
+            return res.status(422).json({error:"Session Expired"})
+        }
+        bcrypt.hash(newPassword,12).then(hashedpassword=>{
+           user.password = hashedpassword
+           user.resetToken = undefined
+           user.expireToken = undefined
+           user.save().then((saveduser)=>{
+               res.json({message:"Password Changed Successfully"})
+           })
+        })
+    }).catch(err=>{
+        console.log(err)
+    })
+})
+
+module.exports = router
